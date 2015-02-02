f8d78da818b23f4fb2585d18621d44f892382d50
Knovour <knovourimba@gmail.com>
2015-02-02 11:58:58 +0800

    update critical error in readme and index.js

--- demo/steam_Dota2.js
+++ demo/steam_Dota2.js
@@ -4,46 +4,46 @@ var setting = {
   type: 'content',
   container: '#game_highlights .rightcol',
   keys: [{
-  	name: 'appId',
-  	elem: '.glance_tags',
-  	get:  ['data', 'appid']
+    name: 'appId',
+    elem: '.glance_tags',
+    get:  ['data', 'appid']
   }, {
     name: 'name',
     outOfContainer: true,
     elem: '.apphub_AppName',
     get:  'text'
   }, {
-  	name: 'image',
-  	elem: '.game_header_image_full',
-  	get:  ['attr', 'src']
+    name: 'image',
+    elem: '.game_header_image_full',
+    get:  ['attr', 'src']
   }, {
     name: 'reviews',
     elem: '.game_review_summary',
     get:  'text',
   }, {
-  	name: 'tags',
-  	elem: '.glance_tags',
-  	collect: {
-  		elems: [{
-  			elem: 'a.app_tag:eq(0)',
-  			get:  'text'
-  		}, {
-  			elem: 'a.app_tag:eq(1)',
-  			get:  'text'
-  		}, {
-  			elem: 'a.app_tag:eq(2)',
-  			get:  'text'
-  		}],
-  		combineWith: ', '
-  	}
+    name: 'tags',
+    elem: '.glance_tags',
+    collect: {
+      elems: [{
+        elem: 'a.app_tag:eq(0)',
+        get:  'text'
+      }, {
+        elem: 'a.app_tag:eq(1)',
+        get:  'text'
+      }, {
+        elem: 'a.app_tag:eq(2)',
+        get:  'text'
+      }],
+      combineWith: ', '
+    }
   }, {
-  	name: 'allTags',
-  	elem: '.glance_tags a.app_tag',
-  	collect: {
-  		loop: true,
-  		get:  'text',
-  		combineWith: ', '
-  	}
+    name: 'allTags',
+    elem: '.glance_tags a.app_tag',
+    collect: {
+      loop: true,
+      get:  'text',
+      combineWith: ', '
+    }
   }, {
     name: 'description',
     elem: '.game_description_snippet',
@@ -60,7 +60,7 @@ var Crawler = require('../index');
 
 require('./lib/requestPage')(url, function(err, content) {
   if(content) {
-    Crawler.start(setting, content, function(err, result) {
+    Crawler.start(content, setting, function(err, result) {
       if(err)
         console.log('Err: ' + err);
 
